6e895ba956e95b5f16e6e70a880e60c82629cf9e
r3dwh33lbarrow <morgan.t30@icloud.com>
2025-08-19 18:38:00 -0500

    Added basic tests. Still failling due to incorrect server/backend database login.

--- client/src/schemas/mod.rs
+++ client/src/schemas/mod.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct RootResponse {
-    message: String,
+    pub message: String,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
