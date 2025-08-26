bf53735ee39e57d3886f4304dc729bb7d09b7e20
r3dwh33lbarrow <morgan.t30@icloud.com>
2025-08-26 15:15:42 -0500

    style: normalize frontend import formatting

--- server/frontend/src/App.tsx
+++ server/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import LoginPanel from "./componets/LoginPanel.tsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Login, Register} from "./services/authentication.ts";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login, Register } from "./services/authentication.ts";
 import RegisterPanel from "./componets/RegisterPanel.tsx";
 import Dashboard from "./componets/Dashboard.tsx";
 import ProtectedRoute from "./componets/ProtectedRoute.tsx";
@@ -9,16 +9,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<LoginPanel onSubmit={Login}/>}/>
-        <Route path='/register' element={<RegisterPanel onSubmit={Register}/>}/>
-        <Route path='/dashboard' element={
+        <Route path="/login" element={<LoginPanel onSubmit={Login} />} />
+        <Route path="/register" element={<RegisterPanel onSubmit={Register} />} />
+        <Route path="/dashboard" element={
           <ProtectedRoute>
             <Dashboard />
           </ProtectedRoute>
-        }/>
+        } />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
