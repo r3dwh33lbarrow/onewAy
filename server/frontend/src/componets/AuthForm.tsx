d324fcb0b1db8ce26ab090cce5c4f89c9bb71aed
r3dwh33lbarrow <morgan.t30@icloud.com>
2025-08-26 15:05:36 -0500

    chore: enforce frontend formatting

--- server/frontend/src/componets/AuthForm.tsx
+++ server/frontend/src/componets/AuthForm.tsx
@@ -54,7 +54,7 @@ export default function AuthForm({
       const success = await onSubmit(formData);
       if (success) {
         if (successRedirectPath === '/dashboard') {
-          setUser({ username: formData.username })
+          setUser({ username: formData.username });
           const from = location.state?.from?.pathname || '/dashboard';
           navigate(from, { replace: true });
         } else {
