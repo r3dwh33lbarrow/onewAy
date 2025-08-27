b2fa5e9e04539716ac1f8511fc7624dff899a065
r3dwh33lbarrow <morgan.t30@icloud.com>
2025-08-27 08:43:45 -0500

    implement authentication redirect in App component and optimize API error handling

--- server/frontend/src/App.tsx
+++ server/frontend/src/App.tsx
@@ -1,24 +1,30 @@
 import LoginPanel from "./componets/LoginPanel.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Login, Register } from "./services/authentication.ts";
-import RegisterPanel from "./componets/RegisterPanel.tsx";
-import Dashboard from "./componets/Dashboard.tsx";
-import ProtectedRoute from "./componets/ProtectedRoute.tsx";
+  import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+  import { Login, Register } from "./services/authentication.ts";
+  import RegisterPanel from "./componets/RegisterPanel.tsx";
+  import Dashboard from "./componets/Dashboard.tsx";
+  import ProtectedRoute from "./componets/ProtectedRoute.tsx";
+  import {useAuthStore} from "./stores/authStore.ts";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPanel onSubmit={Login} />} />
-        <Route path="/register" element={<RegisterPanel onSubmit={Register} />} />
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+  function App() {
+    const isAuthenticated = useAuthStore(state => state.isAuthenticated);
 
-export default App;
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <Navigate to={isAuthenticated ? "/dashboard" : "/login"} replace />
+          } />
+          <Route path="/login" element={<LoginPanel onSubmit={Login} />} />
+          <Route path="/register" element={<RegisterPanel onSubmit={Register} />} />
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+
+  export default App;
