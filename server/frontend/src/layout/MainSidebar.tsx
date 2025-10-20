1a73f8e9074bab519825fb53a1a6f3697b995d85
r3dwh33lbarrow <morgan.t30@icloud.com>
2025-10-20 19:57:56 +0100

    Enhance MainSidebar for dark mode support with updated text and border colors

--- server/frontend/src/layout/MainSidebar.tsx
+++ server/frontend/src/layout/MainSidebar.tsx
@@ -16,9 +16,9 @@ export default function MainSidebar({ onNavigate }: Props) {
     <Sidebar aria-label="Main sidebar" theme={customSidebarTheme}>
       <div className="flex items-center justify-center pb-2 pr-4 pl-4">
         <img src="/onewAy_logo.png" alt="onewAy logo" className="h-10 w-auto" />
-        <p className="ml-3 text-3xl">onewAy</p>
+        <p className="ml-3 text-3xl dark:text-white">onewAy</p>
       </div>
-      <hr className="border-t border-gray-400 mb-2.5 mt-[3px]" />
+      <hr className="border-t border-gray-400 dark:border-gray-700 mb-2.5 mt-[3px]" />
 
       <SidebarItems>
         <SidebarItemGroup>
